f50ac51c14c58123ef1c51a0e14ffab6a81ec1c0
pritesh <priteshvishwakarma@gmail.com>
2020-08-24 20:16:36 +0530

    first commit

---
+++ app/src/main/java/com/example/cardinput2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cardinput2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("", "")
+
+    }
+}
